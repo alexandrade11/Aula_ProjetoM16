0922845e8ff93d70c72dade076c350779f50b5c1
tomás <tomas.leitao27229@al.aememartins.pt>
2025-02-04 16:00:55 +0000

    aa

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mysql = require('mysql2');
 const axios = require('axios');
 const app = express();
 const port = 3000;
@@ -8,8 +7,9 @@ app.use(express.json());
 
 app.set('view engine','ejs');
 
-app.get('/', (res, req) =>{
-    res.render('index', {mensagem: "Olá"});
+app.get('/', (req, res) =>{
+  let  compras = ["Maçã","Compal","Batata"];
+    res.render('index', {mensagem: "A minha lista", lista: compras});
 })
 
 app.listen(port, () => {
